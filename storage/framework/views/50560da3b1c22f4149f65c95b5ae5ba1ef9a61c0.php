ba13e81892370489b34d964460884f2faf57b033
Rohan Sable <39899398+dadiRohan@users.noreply.github.com>
2024-09-27 17:38:53 +0530

    Add files via upload

---
+++ storage/framework/views/50560da3b1c22f4149f65c95b5ae5ba1ef9a61c0.php
@@ -0,0 +1,15 @@
+
+
+<?php $__env->startSection('content'); ?>
+    <!-- <div class="bg-light p-5 rounded">
+        <?php if(auth()->guard()->check()): ?>
+        <h1>Dashboard</h1>
+        <?php endif; ?>
+
+        <?php if(auth()->guard()->guest()): ?>
+        <span>Homepage</span>
+        <?php endif; ?>
+
+    </div> -->
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app-master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\Socio-API\resources\views/home/index.blade.php ENDPATH**/ ?>
