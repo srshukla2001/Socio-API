69e529fbfd19340541a20f867a9facd4e506fae8
Rohan Sable <39899398+dadiRohan@users.noreply.github.com>
2024-10-01 18:42:38 +0530

    Like, Comment APIs

---
+++ database/migrations/2024_09_30_064429_create_likes_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('likes', function (Blueprint $table) {
+            $table->id();
+            $table->integer('post_id');
+            $table->integer('like_count');
+            $table->timestamps();
+        });
+
+        Schema::create('post_likes',function (Blueprint $table){
+            $table->id();
+            $table->integer('like_id');
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('likes');
+        Schema::dropIfExists('post_likes');
+    }
+};
